a8a60352923db3cf3d57f17cfb83b50f81547c30
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-16 01:00:45 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,7 +31,6 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     movie.title = args[:title]
     movie.release_date = args[:release_date]
-    end
   end
 end
 
